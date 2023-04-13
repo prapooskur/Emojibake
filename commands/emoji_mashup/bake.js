5de820cd087b0dc4a6e12fa4b15abc0748052507
prapooskur <prapooskur@gmail.com>
2023-04-13 06:16:43 +0000

    upd

--- commands/emoji_mashup/bake.js
+++ commands/emoji_mashup/bake.js
@@ -21,33 +21,45 @@ module.exports = {
 	console.log(base_emoji);
         console.log(secondary_emoji);
 
-	codepoints=Array.from(base_emoji).map(c => c.codePointAt(0).toString(16));
-	console.log(codepoints);
+	codepoints_base=Array.from(base_emoji).map(c => c.codePointAt(0).toString(16));
+	console.log(codepoints_base);
+    codepoints_secondary=Array.from(secondary_emoji).map(c => c.codePointAt(0).toString(16));
+	console.log(codepoints_secondary);
 	
 	converted_base = ""
-	for (let i = 0; i < codepoints.length; i++) {
+	for (let i = 0; i < codepoints_base.length; i++) {
 	    converted_base+="u";
-	    converted_base+=codepoints[i];
-	    if (i<codepoints.length-1) {
-		converted_base+="-";
-            }
+	    converted_base+=codepoints_base[i];
+	    if (i<codepoints_base.length-1) {
+		    converted_base+="-";
+        }
 	}
 	console.log(converted_base);
+
+    converted_secondary = ""
+	for (let i = 0; i < codepoints_secondary.length; i++) {
+	    converted_secondary+="u";
+	    converted_secondary+=codepoints_secondary[i];
+	    if (i<codepoints_secondary.length-1) {
+		    converted_secondary+="-";
+        }
+	}
+	console.log(converted_secondary);
 	
-	converted_base = base_emoji.codePointAt(0).toString(16);
-        converted_secondary = secondary_emoji.codePointAt(0).toString(16);
+//    converted_base = base_emoji.codePointAt(0).toString(16);
+//    converted_secondary = secondary_emoji.codePointAt(0).toString(16);
 
-        imgpath = "./stickers/u"+converted_base+"_u"+converted_secondary+".png"
-        console.log(imgpath)
+    imgpath = "./stickers/"+converted_base+"_"+converted_secondary+".png"
+    console.log(imgpath)
 
-        try {
-            const image = new AttachmentBuilder(imgpath,"img.png")
-            await interaction.reply({ files : [image] })
-          } catch (error) {
-            await interaction.reply({ content: "The combination you sent did not work.", ephemeral: true })
-            // Expected output: ReferenceError: nonExistentFunction is not defined
-            // (Note: the exact output may be browser-dependent)
-          }
+    try {
+        const image = new AttachmentBuilder(imgpath,"img.png")
+        await interaction.reply({ files : [image] })
+    } catch (error) {
+        await interaction.reply({ content: "The combination you sent did not work.", ephemeral: true })
+        // Expected output: ReferenceError: nonExistentFunction is not defined
+        // (Note: the exact output may be browser-dependent)
+    }
         
 
 //        await interaction.reply(converted_base+"  "+converted_secondary);
